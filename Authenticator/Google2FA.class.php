266b0ff82ddbaa0a152bf3ca0df37c4e28aa0ea7
nuxsmin <nuxsmin@syspass.org>
2017-08-16 09:10:06 +0200

    [MOD] Updated copyright

--- Authenticator/Google2FA.class.php
+++ Authenticator/Google2FA.class.php
@@ -1,28 +1,5 @@
 <?php
 /**
- *  sysPass-Authenticator
- *
- * @author nuxsmin
- * @link http://syspass.org
- * @copyright 2012-2017, Rubén Domínguez nuxsmin@syspass.org
- *
- * This file is part of sysPass-Authenticator.
- *
- * sysPass-Authenticator is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * sysPass-Authenticator is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with sysPass-Authenticator. If not, see <http://www.gnu.org/licenses/>.
- */
-
-/**
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
