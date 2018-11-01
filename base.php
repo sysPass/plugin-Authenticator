5f1d3c5b7706781a50cf4d16d83530946dc29241
nuxsmin <nuxsmin@syspass.org>
2018-11-01 19:10:27 +0100

    * [MOD] Removed autoloader registering

--- base.php
+++ base.php
@@ -7,12 +7,4 @@ $base = [
     'dir' => $lib
 ];
 
-/** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require APP_ROOT . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-$loader->addPsr4($base['namespace'], $base['dir']);
-$loader->addClassMap([
-    \SP\Modules\Web\Controllers\AuthenticatorController::class => $lib . DIRECTORY_SEPARATOR . 'Controllers' . DIRECTORY_SEPARATOR . 'AuthenticatorController.php',
-    \SP\Modules\Web\Controllers\AuthenticatorLoginController::class => $lib . DIRECTORY_SEPARATOR . 'Controllers' . DIRECTORY_SEPARATOR . 'AuthenticatorLoginController.php'
-]);
-
 return $base;
