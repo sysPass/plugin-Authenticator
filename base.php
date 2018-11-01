893ee334efbe0ad68ede8e50832e2f758de16f76
nuxsmin <nuxsmin@syspass.org>
2018-11-01 23:54:30 +0100

    * [MOD] Added autoloader registering as fallback * [ADD] Added QR code generation library

--- base.php
+++ base.php
@@ -7,4 +7,14 @@ $base = [
     'dir' => $lib
 ];
 
+if (!class_exists(\SP\Modules\Web\Plugins\Authenticator\Plugin::class)) {
+    /** @var \Composer\Autoload\ClassLoader $loader */
+    $loader = require APP_ROOT . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+    $loader->addPsr4($base['namespace'], $base['dir']);
+    $loader->addClassMap([
+        \SP\Modules\Web\Controllers\AuthenticatorController::class => $lib . DIRECTORY_SEPARATOR . 'Controllers' . DIRECTORY_SEPARATOR . 'AuthenticatorController.php',
+        \SP\Modules\Web\Controllers\AuthenticatorLoginController::class => $lib . DIRECTORY_SEPARATOR . 'Controllers' . DIRECTORY_SEPARATOR . 'AuthenticatorLoginController.php'
+    ]);
+}
+
 return $base;
